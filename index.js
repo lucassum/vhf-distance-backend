edbbfb2d4322b20eb06d356dba3ab902b103c852
Lucas Jórdan <lucasjordandt@gmail.com>
2021-11-07 16:19:05 -0300

    antennas post

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port port!`))
+const antennas = []
+
+app.use(cors())
+app.use(morgan('dev'))
+app.get('/', (req, res) => 'teste')
+
+app.post('/', (req, res) => {
+    antennas.push(req.body)
+    return antennas;
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))

6d1b5cfa007faa13c784c86b1fbb6869a3f793bb
Lucas Jórdan <lucasjordandt@gmail.com>
2021-11-07 21:28:01 -0300

    eraseall

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const morgan = require('morgan')
 const app = express()
 const port = process.env.PORT || 3000
 
-const antennas = []
+let antennas = []
 
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.get('/', (req, res) => res.json(antennas))
-
+app.delete('/', (req, res) => {
+    antennas = []
+    res.json(antennas)
+})
 app.post('/', (req, res) => {
     const { height } = req.body
     const distance = Math.sqrt(12.76 * Number(height))
